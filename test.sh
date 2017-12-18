f3a1fa4a5d8f914e6959664313a51099acb5b94b
Krishnaswamy, Bharath <Bharath.Krishnaswamy@adidas-group.com>
2017-12-18 15:41:37 +0100

    added k8s for test and server

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ set -eu
 ID=$! # ID of webserver process, so we can kill it
 
 tests_passed=true
-expected="Hello From Kubernetes in CI/CD Training."
+expected="Hello From Kubernetes in CI/CD Training with k8s scaling."
 output=$(curl -s localhost:8080)
 if [[ $output == *"$expected"* ]]; then
   echo "Test Success"
